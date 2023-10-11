84194c51e76bb09245d21f8cb7d1822abf2cd740
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-11 15:03:08 -0400

    Event handler

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ const messages = [
 export default function App(){
   // Variable that specifies current step:
   const step = 1;
+
+  function handlePrevious(){
+      alert("Previous");
+  }
+  function handleNext(){
+      alert("Next");
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -18,8 +26,10 @@ export default function App(){
       <p className="message">Step {step}: {messages[step - 1]}</p>
 
       <div className="buttons">
-        <button style={{backgroundColor: "#7950f2", color: "#fff"}}>Previous</button>
-        <button style={{backgroundColor: "#7950f2", color: "#fff"}}>Next</button>
+        <button style={{backgroundColor: "#7950f2", color: "#fff"}}
+        onClick={handlePrevious}>Previous</button>
+        <button style={{backgroundColor: "#7950f2", color: "#fff"}} 
+        onClick={handleNext}>Next</button>
       </div>
     </div>
   );
