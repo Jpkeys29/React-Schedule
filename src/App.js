78cded9fa217f0c23b637983b710f09805807498
Jp Keys <jpmusik29@gmail.com>
2024-01-09 07:06:25 -0500

    Styling the reusable button

--- src/App.js
+++ src/App.js
@@ -29,11 +29,13 @@ export default function App(){
             <div className={step >=3 ? "active" : ""}>3</div>
           </div>
 
-          <StepMessage step={1}>{messages[step -1]}
-          <Button bgColor='#e7e7e7' textColor='#333'
-          onClick={() => alert(`Learn how to ${messages[step -1]}`)}>
-            Learn how 
-          </Button>
+          <StepMessage step={step}>{messages[step -1]}
+          <div className="buttons">
+            <Button bgColor='#e7e7e7' textColor='#333'
+            onClick={() => alert(`Learn how to ${messages[step -1]}`)}>
+              Learn how 
+            </Button>
+          </div>
           </StepMessage>
 
           <div className="buttons">
